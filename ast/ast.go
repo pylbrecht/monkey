067c6f2e990e933e84c47a3061fba7ae046cfb10
Philipp Albrecht <palbrecht@mailbox.org>
2022-08-25 21:48:22 +0200

    Parse integer literal expressions

--- ast/ast.go
+++ ast/ast.go
@@ -59,6 +59,19 @@ func (rs *ReturnStatement) String() string {
 	return out.String()
 }
 
+type IntegerLiteral struct {
+	Token token.Token // the token.INT token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode() {}
+func (il *IntegerLiteral) TokenLiteral() string {
+	return il.Token.Literal
+}
+func (il *IntegerLiteral) String() string {
+	return il.Token.Literal
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
